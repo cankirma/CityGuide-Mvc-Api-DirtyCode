f2691a913d2f72008a01ffe6a26489889c3505c1
Mustafa Can Kırma <cankirma@gmail.com>
2021-01-19 03:35:52 +0300

    Add project files.

---
+++ api/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
